abf04e637bea40d0f4e0c7425a0f52ffa195bffc
dimastamer1 <dimaolx52538@gmail.com>
2025-06-10 13:09:34 +0300

    тест газов

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ export default {
   telegramToken: process.env.TELEGRAM_TOKEN,
   cryptoBotToken: process.env.CRYPTO_BOT_TOKEN,
   adminId: Number(process.env.ADMIN_ID),
+  botUsername: process.env.BOT_USERNAME, // <-- добавлено для генерации реферальной ссылки
   imap: {
     user: process.env.IMAP_USER,
     password: process.env.IMAP_PASSWORD,
